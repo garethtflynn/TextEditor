35a8c31c4e202d5e0f34f955318982d9b2763a47
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-11-16 15:39:58 -0500

    added proper manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,9 +27,23 @@ module.exports = () => {
         swDest: 'src-sw.js'
       }),
       new WebpackPwaManifest({
-
-      })
-
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
       module: {
       rules: [
